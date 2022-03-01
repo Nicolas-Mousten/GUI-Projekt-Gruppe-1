181d77593a99757db0c91a562fd086cfd7fdefd5
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-03-01 10:54:31 +0100

    Drop down menu

--- Web/src/main/java/dmm/web/PageController.java
+++ Web/src/main/java/dmm/web/PageController.java
@@ -1,8 +1,12 @@
 package dmm.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-
+    @GetMapping("/")
+    public String mainPage(){
+        return "mainPage";
+    }
 }
