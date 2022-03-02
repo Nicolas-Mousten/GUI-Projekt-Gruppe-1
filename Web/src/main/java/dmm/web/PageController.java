3e6a5ffd9bfebd0e799e8469810f5c7948176ea9
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-03-02 14:32:36 +0100

    Center drop menu

--- Web/src/main/java/dmm/web/PageController.java
+++ Web/src/main/java/dmm/web/PageController.java
@@ -17,4 +17,8 @@ public class PageController {
     public String DonatePage(){
         return "donatePage";
     }
+    @GetMapping("/underConstructionPage")
+    public String UnderConstructionPage(){
+        return "UnderConstructionPage";
+    }
 }
